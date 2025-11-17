fb128090a9c8828533e25d66e13f3207b36dccf5
Simone Massonie <simone.massonie@gmail.com>
2025-11-17 13:01:37 +0100

    add leaflet

--- src/main.js
+++ src/main.js
@@ -1,24 +1,50 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import "./style.css";
+import * as Leaflet from "leaflet";
+import "leaflet/dist/leaflet.css"; // import leaflet css
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const WBS = [52.457131, 13.54007]; // WBS coordinates
+const map = Leaflet.map("map").setView(WBS, 13); // create a map object with a center and zoom level
+const markerIcon = Leaflet.icon({
+  iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",
+  iconAnchor: [10, 20],
+}); // There was an issue with the default marker icon, so we create a new one
+Leaflet.marker(WBS, { icon: markerIcon }).addTo(map); // add a marker to the map at the WBS coordinates
 
-setupCounter(document.querySelector('#counter'))
+Leaflet.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  maxZoom: 19,
+  attribution: '&copy; <a href="https://google.com" target="_blank">Google</a>',
+}).addTo(map); // add a tile layer to the map, the tiles are those images that make up the map
+
+const myLocations = [
+  {
+    name: "WBS CODING SCHOOL",
+    location: [52.457131, 13.54007],
+    description: "The best coding school in the world",
+  },
+  {
+    name: "Alexanderplatz",
+    location: [52.521918, 13.413215],
+    description: "The most famous square in Berlin",
+  },
+  {
+    name: "Brandenburg Gate",
+    location: [52.516275, 13.377704],
+    description: "The most famous gate in Berlin",
+  },
+  {
+    name: "Berlin Wall",
+    location: [52.507541, 13.39032],
+    description: "The most famous wall in Berlin",
+  },
+];
+
+myLocations.forEach((location) => {
+  Leaflet.marker(location.location, { icon: markerIcon })
+    .bindPopup(location.description)
+    .addTo(map);
+});
+
+const bounds = Leaflet.latLngBounds(
+  myLocations.map((location) => location.location)
+);
+map.fitBounds(bounds);
